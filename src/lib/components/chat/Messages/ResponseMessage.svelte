211ada474405158627ea559dc4714aa45cce5bfe
DenysYefimovTeva <denys.yefimov@teva.ua>
2026-05-05 14:03:50 +0300

    Update ResponseMessage.svelte

--- src/lib/components/chat/Messages/ResponseMessage.svelte
+++ src/lib/components/chat/Messages/ResponseMessage.svelte
@@ -661,7 +661,7 @@
 			<div>
 				<div class="chat-{message.role} w-full min-w-full markdown-prose">
 					<div>
-						{#if model?.info?.meta?.capabilities?.status_updates ?? true}
+						{#if $user?.role === 'admin' && (model?.info?.meta?.capabilities?.status_updates ?? true)}
 							<StatusHistory statusHistory={message?.statusHistory} />
 						{/if}
 
